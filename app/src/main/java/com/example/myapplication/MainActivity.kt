f2fb0f443270a1aee081220cdbeacfd6c380a65b
Vladislav Chelnakov <f1217@bk.ru>
2021-07-12 16:06:20 +0300

    12

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,28 +1,21 @@
-package com.fittin.fa4Lib
+package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class Finder : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
-        findViewById<TextView>(R.id.sample_text).text = GetVersion()
+        findViewById<TextView>(R.id.sample_text).text = com.fittin.fa4Lib.Finder.GetVersion()
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun GetVersion(): String
 
-    companion object {
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("Fa4-native-lib")
-        }
-    }
 }
---
+++ app/src/main/java/com/fittin/fa4Lib/Finder.kt
@@ -0,0 +1,9 @@
+package com.fittin.fa4Lib
+
+class Finder {
+
+
+    companion object {
+        @JvmStatic external fun GetVersion(): String
+    }
+}
